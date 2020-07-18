7273fe9f837807ffc0e1e74dc804d29fb73a9739
preuschhi <bmx.lego@gmail.com>
2020-07-18 15:15:30 +0200

    Update Spiel.java

--- ErsterVersuch/src/Spiel.java
+++ ErsterVersuch/src/Spiel.java
@@ -1,38 +1,49 @@
 
+
 public class Spiel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Spieler spieler = new Spieler("Leon", 100); // spieler wird erstellt
+		boolean spielBeenden = false;
+		Spieler spieler = new Spieler("Leon", 100); //Spielerobjekt wird erstellt
+		Spieler spielerID = spieler;
+		StandartSchwert waffe = new StandartSchwert();//Waffenobjekt als Startwaffe
+		spieler.setWaffe(waffe); // dem spieler wird die  Standartwaffe zugewiesen
+		
+		
+		//Hier wird das Array erstellt
+		CustomArray [] inventar = new CustomArray[2];
+		//Heiltrank
+		inventar[0] = new CustomArray();
+		CustomArray heiltrankID = inventar[0];
+
+		inventar[0].setObjectName("Heiltank");
+		inventar[0].setWertID(2);
+
+		
+		//Gold
+		inventar[1] = new CustomArray();
+		CustomArray goldID = inventar[1];
+
+		inventar[1].setObjectName("Gold");
+		inventar[1].setWertID(10200);
+
+		
+		
+		do {
+			spielBeenden = spieler.auswahlMethode(goldID, heiltrankID, spielerID);
+		} while (spielBeenden == false);
+		
+		System.out.println("Du hast das Spiel beendet!");
+		
+		
+		
+		
+		
+		
+		
 	
-		
-		
-		
-		StandartSchwert waffe = new StandartSchwert();
-		spieler.setWaffe(waffe); // dem spieler wird eine waffe zugewiesen
-		
-		spieler.angriff(spieler);
-		
-		
-		
-		
-		
-		
-			 
-		
-		
-	
-				
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		
 		
 		
