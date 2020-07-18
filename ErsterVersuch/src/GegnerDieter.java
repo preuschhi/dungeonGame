7e06f3b886930a9e1cc7c1079fd4cfc9286d531e
preuschhi <bmx.lego@gmail.com>
2020-07-18 15:15:20 +0200

    Update GegnerDieter.java

--- ErsterVersuch/src/GegnerDieter.java
+++ ErsterVersuch/src/GegnerDieter.java
@@ -1,17 +1,19 @@
 
 public class GegnerDieter extends Gegner{
 	
-	Waffe waffeGegnerDieter;
-	double spielerWaffeHaltbarkeitAbzug = 1.5;
+	int goldFuerSpieler = 20; //So viel Gold bekommt der Spieler wenn er diesen Gegner tötet
+	double spielerWaffeHaltbarkeitAbzug = 1.5; //So viel wird der Waffe nach dem tötet dieses Gegenrs abgezogen
 	
 	//Konstruktor
 	public GegnerDieter() {
-		super("King Dieter", 60);
+		super("King Dieter", 70);
 		StarkesSchwert waffeGegnerDieter = new StarkesSchwert();
 		this.waffeGegner = waffeGegnerDieter;
 	}
 
 	//Methoden
+
+	
 	@Override
 	public void angriff() {
 		// TODO Auto-generated method stub
